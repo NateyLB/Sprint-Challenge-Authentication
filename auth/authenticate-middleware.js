520a4f63263049c90a75ee36fa81e329bfeeca5a
Nathan <nateyl.b@gmail.com>
2020-05-22 12:10:04 -0700

    finished up testing

--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -7,7 +7,6 @@ var jwtDecode = require('jwt-decode');
 const Users = require('../auth/users-model.js')
 
 module.exports = (req, res, next) => {
-  console.log(req.headers)
   var decoded = jwtDecode(req.headers.authorization)
   Users.findBy({ "users.username": decoded.username })
       .then(([user]) => {
@@ -17,5 +16,9 @@ module.exports = (req, res, next) => {
           } else {
             res.status(401).json({ you: 'shall not pass!' });
           }
+      })
+      .catch(err=>{
+        console.log(err)
+        res.status(401).json({ message: 'you shall not pass!' });
       })
 };
