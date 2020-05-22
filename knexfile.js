062df2e0fde31cdaa351fe864c3861691b7bee3c
Nathan <nateyl.b@gmail.com>
2020-05-22 10:41:41 -0700

    set up project

--- knexfile.js
+++ knexfile.js
@@ -2,6 +2,11 @@ module.exports = {
   development: {
     client: 'sqlite3',
     connection: { filename: './database/auth.db3' },
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run("PRAGMA foreign_keys = ON", done);
+      },
+    },
     useNullAsDefault: true,
     migrations: {
       directory: './database/migrations',
@@ -9,4 +14,39 @@ module.exports = {
     },
     seeds: { directory: './database/seeds' },
   },
+
+testing: {
+  client: "sqlite3",
+  connection: {
+    filename: "./data/test.db3",
+  },
+  pool: {
+    afterCreate: (conn, done) => {
+      conn.run("PRAGMA foreign_keys = ON", done);
+    },
+  },
+  useNullAsDefault: true,
+  migrations: {
+    directory: "./database/migrations",
+  },
+  seeds: {
+    directory: "./database/seeds",
+  },
+},
+
+// heroku postgres
+production: {
+  client: "pg", // npm i pg
+  connection: pgConnection,
+  pool: {
+    min: 2,
+    max: 10,
+  },
+  migrations: {
+    directory: "./database/migrations",
+  },
+  seeds: {
+    directory: "./database/seeds",
+  },
+},
 };
